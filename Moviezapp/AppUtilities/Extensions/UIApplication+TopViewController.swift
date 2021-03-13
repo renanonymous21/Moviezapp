4ee82f537e64deb51b74f098dbfaee60a0b326d8
Rendy K. R <rendy.krisna@cashbac.com>
2021-03-13 16:43:22 +0700

    minor fixing

--- Moviezapp/AppUtilities/Extensions/UIApplication+TopViewController.swift
+++ Moviezapp/AppUtilities/Extensions/UIApplication+TopViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 extension UIApplication {
     class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
 
-        if let nav = base as? UINavigationController {
-            return topViewController(base: nav.visibleViewController)
+        if let navController = base as? UINavigationController {
+            return topViewController(base: navController.visibleViewController)
 
-        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+        } else if let tabBarController = base as? UITabBarController, let selected = tabBarController.selectedViewController {
             return topViewController(base: selected)
 
         } else if let presented = base?.presentedViewController {
